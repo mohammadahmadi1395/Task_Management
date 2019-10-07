551eb6ecf5d93401e91c9096b6cedb157a05576f
Ruhollah Ahmadi <roohix.code@gmail.com>
2019-10-07 18:17:17 +0430

    Improvement : update readme ; admin rtl and some stuff (#1)
    * Improvement : update readme ; admin rtl and some stuff
    
    * readme next line

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Task_Management
 This project is for python course in Kateb university in Kabul
+
+Step of installation : 
+
+1- clone the project 
+In terminal :
+```
+git clone https://github.com/mohammadahmadi1395/Task_Management.git
+```
+
+2- install requirements 
+Terminal
+```
+pip3 install virtualenv
+virtualenv .venv
+pip3 install django
+```
+
+3- run django
+```
+cd tasksite
+python3 manage.py runserver
+```
+
+admin area :
+```
+http://127.0.0.1:8000/admin/
+username : admin
+password : admin
+```
+
+
+enjoy !
--- tasksite/tasks/urls.py
+++ tasksite/tasks/urls.py
@@ -6,9 +6,9 @@ urlpatterns = [
     # ex: /tasks/
     path('', views.index, name='index'),
     # ex: /tasks/5/
-    path('<int:question_id>/', views.detail, name='detail'),
+    path('tasks/<int:question_id>/', views.detail, name='detail'),
     # ex: /tasks/5/results/
-    path('<int:question_id>/results/', views.results, name='results'),
+    path('tasks/<int:question_id>/results/', views.results, name='results'),
     # ex: /tasks/5/vote/
-    path('<int:question_id>/vote/', views.vote, name='vote'),
+    path('tasks/<int:question_id>/vote/', views.vote, name='vote'),
 ]
